b2a54d61b21f57ead877d801f3c4689c0e73ef6e
Loo Yiyang <yiyangloo.work@gmail.com>
2020-06-23 10:50:09 +0800

    Code Improving #1

--- resources/views/election/show.blade.php
+++ resources/views/election/show.blade.php
@@ -1,29 +1,21 @@
 @extends('layouts.main')
 
+@section('css')
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+    .countdown li {
+        display: inline-block;
+        font-size: 1.5em;
+        list-style-type: none;
+        padding: 1em;
+        text-transform: uppercase;
+    }
 
-h1 {
-  font-weight: normal;
-}
-
-.li {
-  display: inline-block;
-  font-size: 1.5em;
-  list-style-type: none;
-  padding: 1em;
-  text-transform: uppercase;
-}
-
-.li span {
-  display: block;
-  font-size: 4.5rem;
-}
+    .countdown li span {
+        display: block;
+        font-size: 4.5rem;
+    }
 </style>
+@endsection
 
 @section('activity')
 <div class="card">
@@ -37,7 +29,6 @@ h1 {
                 {!! $chart->script() !!}
             </div>
         </div>
-
         <div class="row">
             <div class="col-sm-6">
                 <h5 class="card-title">Start Date and Time</h5>
@@ -47,21 +38,20 @@ h1 {
                 <h5 class="card-title">End Date and Time</h5>
                 <p class="card-text">{{$election->end_date}}</p>
             </div>
-            
         </div>
         <br>
-        <div class="container text-center">
-            <ul>
-                <li class = "li"><span id="days"></span>days</li>
-                <li class = "li"><span id="hours"></span>Hours</li>
-                <li class = "li"><span id="minutes"></span>Minutes</li>
-                <li class = "li"><span id="seconds"></span>Seconds</li>
-            </ul>
-        </div>
         <div class="my-4">
             <h5 class="card-title">Description</h5>
             <p class="card-text">{{$election->election_description}}</p>
-
+        </div>
+        <div class="container text-center countdown">
+            <ul>
+                <li class="li"><span id="expired"></span></li>
+                <li class="li"><span id="days"></span>days</li>
+                <li class="li"><span id="hours"></span>Hours</li>
+                <li class="li"><span id="minutes"></span>Minutes</li>
+                <li class="li"><span id="seconds"></span>Seconds</li>
+            </ul>
         </div>
         <table class="table">
             <thead>
@@ -80,13 +70,9 @@ h1 {
                             data-id="{{$candidate->id}}" data-name="{{$candidate->name}}" data-toggle="modal"
                             data-target="#vote_candidate">Vote</button></td>
                 </tr>
-
                 @endforeach
             </tbody>
         </table>
-    </div>
-    <div class="card-footer text-muted">
-
     </div>
 </div>
 
@@ -118,32 +104,37 @@ h1 {
     </div>
 </div>
 
-<script>
+@section('javascript')
+<script defer>
     $(document).on("click", "#vote_confirmation", function () {
-    let candidate_id = $(this).data('id');
-    let candidate_name = $(this).data('name');
-    $(".modal-body #candidate_name").text( candidate_name );
-    $(".modal-footer #candidate_id").val( candidate_id );
-  });
+        let candidate_id = $(this).data('id');
+        let candidate_name = $(this).data('name');
+        $(".modal-body #candidate_name").text( candidate_name );
+        $(".modal-footer #candidate_id").val( candidate_id );
+    });
+    
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    
+    let countDownDateTime = {{ strtotime("$election->end_date") }} * 1000;
+    
+    let timer = setInterval(function(){
+        let now = new Date().getTime();
+        let timeInterval = countDownDateTime - now;
+        
+        document.getElementById('days').innerText = Math.floor(timeInterval / day);
+        document.getElementById('hours').innerText = Math.floor((timeInterval % day) / hour);
+        document.getElementById('minutes').innerText = Math.floor((timeInterval % hour) / minute);
+        document.getElementById('seconds').innerText = Math.floor((timeInterval % minute) / second);
+        
+        if (timeInterval<0){
+            clearInterval(timer);
+            document.getElementById('expired').innerText = "THE VOTING HAS EXPIRED";
+        }
+    },1000);
 </script>
+@endsection
 
-<script>
-     const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-
-let countDown = <?php echo strtotime("$election->end_date" ) ?> * 1000;
-    x = setInterval(function() {    
-
-      let now = new Date().getTime(),
-          distance = countDown - now;
-
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-
-    }, second)
-</script>
 @endsection
